6641ab286680495ba72252f360c2c7295f43f0bc
TuanDo <doanhtuan3536@gmail.com>
2025-02-23 15:06:24 +0700

    Upload src

---
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -0,0 +1,161 @@
+package com.skyapi.weatherforecast.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+    @Column(length = 12, nullable = false, unique = true)
+    @Id
+//    @NotNull(message = "Location code can not be null")
+//    @Length(min = 3, max = 12, message = "Location code must have 3-12 characters")
+    private String code;
+
+    @Column(length = 128, nullable = false)
+    @JsonProperty("city_name")
+//    @NotNull(message = "City name can not be null")
+//    @Length(min = 3, max = 128, message = "City name must have 3-128 characters")
+    private String cityName;
+
+    @Column(length = 128)
+    @JsonProperty("region_name")
+//    @Length(min = 3, max = 128, message = "Region name must have 3-128 characters")
+    private String regionName;
+
+    @Column(length = 64, nullable = false)
+    @JsonProperty("country_name")
+//    @NotNull(message = "Country name can not be null")
+//    @Length(min = 3, max = 64, message = "Country name must have 3-128 characters")
+    private String countryName;
+
+    @Column(length = 2, nullable = false)
+    @JsonProperty("country_code")
+//    @NotNull(message = "Country code can not be null")
+//    @Length(min = 2, max = 2, message = "Country code must have 2 characters")
+    private String countryCode;
+
+    private boolean enabled;
+
+    @JsonIgnore
+    private boolean trashed;
+
+    @OneToOne(mappedBy = "location", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    @JsonIgnore
+    private RealtimeWeather realtimeWeather;
+
+    @OneToMany(mappedBy = "id.location", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HourlyWeather> listHourlyWeather = new ArrayList<>();
+
+    public List<HourlyWeather> getListHourlyWeather() {
+        return listHourlyWeather;
+    }
+
+    public void setListHourlyWeather(List<HourlyWeather> listHourlyWeather) {
+        this.listHourlyWeather = listHourlyWeather;
+    }
+
+    public Location(){
+
+    }
+
+    public Location(String cityName, String regionName, String countryName, String countryCode) {
+        this.cityName = cityName;
+        this.regionName = regionName;
+        this.countryName = countryName;
+        this.countryCode = countryCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isTrashed() {
+        return trashed;
+    }
+
+    public void setTrashed(boolean trashed) {
+        this.trashed = trashed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return Objects.equals(code, location.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(code);
+    }
+
+    @Override
+    public String toString() {
+        return cityName + ", " + (regionName != null ? regionName + ", " : "")  + countryName;
+    }
+
+    public RealtimeWeather getRealtimeWeather() {
+        return realtimeWeather;
+    }
+
+    public void setRealtimeWeather(RealtimeWeather realtimeWeather) {
+        this.realtimeWeather = realtimeWeather;
+    }
+
+    public Location code(String code){
+        setCode(code);
+        return this;
+    }
+}
